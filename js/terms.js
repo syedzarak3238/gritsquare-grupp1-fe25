a309903a2f6680a991c5a5eea6a94dd85952a7cf
syedzarak3238 <syedzarak3238@gmail.com>
2026-03-25 12:22:49 +0100

    Tagit bort  old HTML och CSS

--- js/terms.js
+++ js/terms.js
@@ -1,24 +1,24 @@
-document.addEventListener('DOMContentLoaded', function() {
-  const popup = document.getElementById('termsPopup');
-  const acceptBtn = document.getElementById('acceptBtn');
-  const declineBtn = document.getElementById('declineBtn');
-  const agreeCheckbox = document.getElementById('agreeCheckbox');
+// document.addEventListener('DOMContentLoaded', function() {
+//   const popup = document.getElementById('termsPopup');
+//   const acceptBtn = document.getElementById('acceptBtn');
+//   const declineBtn = document.getElementById('declineBtn');
+//   const agreeCheckbox = document.getElementById('agreeCheckbox');
 
   
-  popup.style.display = 'flex';
+//   popup.style.display = 'flex';
 
-  // Enable accept button only if checkbox is checked
-  agreeCheckbox.addEventListener('change', function() {
-    acceptBtn.disabled = !this.checked;
-  });
-
-  
-  acceptBtn.addEventListener('click', function() {
-    popup.style.display = 'none';
-  });
+//   // Enable accept button only if checkbox is checked
+//   agreeCheckbox.addEventListener('change', function() {
+//     acceptBtn.disabled = !this.checked;
+//   });
 
   
-  declineBtn.addEventListener('click', function() {
-    window.location.href = 'https://www.google.com';
-  });
-});
+//   acceptBtn.addEventListener('click', function() {
+//     popup.style.display = 'none';
+//   });
+
+  
+//   declineBtn.addEventListener('click', function() {
+//     window.location.href = 'https://www.google.com';
+//   });
+// });
